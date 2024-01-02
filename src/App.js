3ec62663ee8aa86c1e6a49958246da60c41f7ecf
LeeBhin <bini4039@naver.com>
2024-01-02 18:41:34 +0900

    routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Home from "./pages/Home"
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./style.scss"
 
 function App() {
   return (
-    <div>
-      <Register />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom"
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault()
     const displayName = e.target[0].value;
@@ -37,7 +40,10 @@ const Register = () => {
               displayName,
               email,
               photoURL: downloadURL
-            })
+            });
+
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/")
           });
         }
       );
