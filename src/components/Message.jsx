80c49f7f0f0c462c1c6449ba9e7e48c3cd4c20a5
LeeBhin <bini4039@naver.com>
2024-01-02 16:22:16 +0900

    responsive design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const Message =() =>{
-    return(
-        <div>
+const Message = () => {
+    return (
+        <div className='message owner'>
+            <div className="messageInfo">
+                <img src="https://us-tuna-sounds-images.voicemod.net/f7afe41a-a81c-47af-9a1b-52e9b4422c14-1699941235855.png" alt="" />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                <img src="https://us-tuna-sounds-images.voicemod.net/f7afe41a-a81c-47af-9a1b-52e9b4422c14-1699941235855.png" alt="" />
+            </div>
         </div>
     )
 }
