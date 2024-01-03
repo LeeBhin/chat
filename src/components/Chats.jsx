3f10330aadd4ba76bcd148bc16ed83d283057b3a
LeeBhin <bini4039@naver.com>
2024-01-03 16:24:10 +0900

    private chat

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,38 +1,43 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
+
+  const [chats, setChats] = useState([])
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      });
+
+      return () => {
+        unsub();
+      }
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid])
+
+  console.log(chats)
   return (
     <div className="chats">
-      <div className="userChat">
-        <img
-          src="https://us-tuna-sounds-images.voicemod.net/f7afe41a-a81c-47af-9a1b-52e9b4422c14-1699941235855.png"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Doji</span>
-          <p>Hello</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+          <img
+            src={chat[1].userInfo.photoURL}
+            alt=""
+          />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://us-tuna-sounds-images.voicemod.net/f7afe41a-a81c-47af-9a1b-52e9b4422c14-1699941235855.png"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Doji</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://us-tuna-sounds-images.voicemod.net/f7afe41a-a81c-47af-9a1b-52e9b4422c14-1699941235855.png"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Doji</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
