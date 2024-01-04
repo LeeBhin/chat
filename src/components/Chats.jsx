1af9abcbbae5cc73616b8d5e4a4b3e36d2831f03
LeeBhin <bini4039@naver.com>
2024-01-04 11:08:53 +0900

    console

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -30,7 +30,7 @@ const Chats = () => {
 
   return (
     <div className="chats">
-      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+      {Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map((chat) => (
         <div
           className="userChat"
           key={chat[0]}
@@ -40,6 +40,7 @@ const Chats = () => {
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
             <p>{chat[1].lastMessage?.text}</p>
+            {console.log(chat[1].lastMessage?.text)}
           </div>
         </div>
       ))}
